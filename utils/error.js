f9053cf5d92bb11f84f247ab7375cb2f8d4d5cbc
Ty Cobb <ty.cobb.m@gmail.com>
2017-03-15 16:03:14 -0500

    remove error helper call, does not exist in react-native

--- utils/error.js
+++ utils/error.js
@@ -1,7 +1,6 @@
 export default class Auth0Error extends Error {
   constructor (json) {
     super();
-    Error.captureStackTrace(this, this.constructor);
     this.name = json.error || json.code || 'a0.internal.failed';
     this.message = json.description || json.error_description || response.statusText || response.status;
     this.json = json;
